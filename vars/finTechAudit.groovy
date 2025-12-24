ead36a0e4692cc2eaf083df3f3b5ee54ec4b8c04
shahgnp <gauravshahfromnepal@gmail.com>
2025-12-24 23:49:41 +0545

    init: shared library

---
+++ vars/finTechAudit.groovy
@@ -0,0 +1,10 @@
+def call(String appName, String riskLevel) {
+    echo "========================================================"
+    echo "   Trainer Solutions - OFFICIAL AUDIT LOG"
+    echo "========================================================"
+    echo "   Application: ${appName}"
+    echo "   Risk Level : ${riskLevel}"
+    echo "   Timestamp  : ${new Date()}"
+    echo "   Compliance : ENFORCED"
+    echo "========================================================"
+}
